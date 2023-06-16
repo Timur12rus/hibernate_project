5f86ffcaf8022921263c9fa4ce4c6dc30e06e5c2
tim.gordeev2015@yandex.ru <tim.gordeev2015@yandex.ru>
2023-06-16 14:53:22 +0300

    HQL queries examples

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 /**
  * Hello world!
  */
@@ -18,11 +20,22 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person2 = session.get(Person.class, 2);
-            person2.setName("New Person2");
+//            Person person2 = session.get(Person.class, 2);
+//            person2.setName("New Person2");
+//            ********************************************************************
+            // вывести на экран людей, чье имя начинается на 'T' и возраст > 29
+//            List<Person> people = session.createQuery("FROM Person WHERE name like 'T%' and age > 29").getResultList();
+//            for (Person person : people) {
+//                System.out.println(person);
+//            }
+//            ********************************************************************
+            // изменить имена людей, чей возраст меньше 30
+//            session.createQuery("update Person  set name = 'Murka' where age < 30").executeUpdate();
+//            **********************************************************************
+            // Удалить людей, чей возраст меньше 30
+//            session.createQuery("delete from Person where age < 30").executeUpdate();
 
             session.getTransaction().commit();
-
         } finally {
             sessionFactory.close();     // в любом случае мы закроем сессию
         }
